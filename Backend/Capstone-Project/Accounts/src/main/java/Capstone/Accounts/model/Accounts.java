a653145bc0a794eed69ebcfff3b24203d219c0a4
SaiManogna-Git <manognakarnati5@gmail.com>
2026-06-28 21:29:03 +0530

    changed accounts model to IDENTITY

--- Backend/Capstone-Project/Accounts/src/main/java/Capstone/Accounts/model/Accounts.java
+++ Backend/Capstone-Project/Accounts/src/main/java/Capstone/Accounts/model/Accounts.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Accounts {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountId;
 
     private String accountName;
